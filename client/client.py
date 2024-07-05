60ddc091b6800e2aa7dbba4932b611349aa9d2b1
David Buchanan <d@vidbuchanan.co.uk>
2024-07-05 18:04:57 +0100

    hello world

---
+++ client/client.py
@@ -0,0 +1,102 @@
+import asyncio
+from typing import List, Self, Optional
+
+class BackdClientSession():
+	def __init__(self, rhost:str, rport:int) -> None:
+		self.rhost = rhost
+		self.rport = rport
+
+		# make sure we only ever have one request in-flight at a time
+		self.request_lock = asyncio.Lock()
+
+	async def connect(self) -> None:
+		self.reader, self.writer = await asyncio.open_connection(
+			self.rhost, self.rport
+		)
+		server_info_len = int.from_bytes(await self.reader.readexactly(4), "little")
+		server_info = await self.reader.readexactly(server_info_len)
+		self.buf_len = int.from_bytes(server_info[:4], "little")
+		self.buf_addr = int.from_bytes(server_info[4:12], "little")
+		print("buffer @", hex(self.buf_addr))
+
+	async def shutdown(self):
+		self.writer.close()
+		await self.writer.wait_closed()
+
+	async def do_request(self, cmd_id: int, header: bytes, body: bytes):
+		async with self.request_lock:
+			msg = (
+				cmd_id.to_bytes(2, "little") +
+				len(header).to_bytes(2, "little") +
+				len(body).to_bytes(4, "little") +
+				header + body
+			)
+			self.writer.write(msg)
+			await self.writer.drain()
+			response_len = int.from_bytes(await self.reader.readexactly(4), byteorder="little", signed=True)
+			if response_len < 0:
+				raise Exception("request failed")
+			response = await self.reader.readexactly(response_len)
+			return response
+	
+	async def cmd_write_mem(self, addr: int, buf: bytes):
+		res = await self.do_request(
+			cmd_id=0,
+			header=addr.to_bytes(8, "little"),
+			body=buf
+		)
+		assert(len(res) == 0)
+	
+	async def cmd_read_mem(self, addr: int, length: int):
+		res = await self.do_request(
+			cmd_id=1,
+			header=addr.to_bytes(8, "little") + length.to_bytes(4, "little"),
+			body=b""
+		)
+		assert(len(res) == length)
+		return res
+	
+	async def cmd_raw_syscall(self, sys_no: int, *args):
+		res = await self.do_request(
+			cmd_id=2,
+			header=sys_no.to_bytes(2, "little") + b"".join(arg.to_bytes(8, "little") for arg in args),
+			body=b""
+		)
+		assert(len(res) == 8)
+		res = int.from_bytes(res, byteorder="little", signed=True)
+		return res
+	
+	async def cmd_syscall_helper(self, sys_no: int, *args):
+		buffer = b""
+		int_args = []
+		for arg in args:
+			match arg:
+				case int():
+					int_args.append(arg & 0xffff_ffff_ffff_ffff)
+				case bytes():
+					int_args.append(self.buf_addr + len(buffer))
+					buffer += arg
+				case str():
+					int_args.append(self.buf_addr + len(buffer))
+					buffer += arg.encode() + b"\x00"
+		if buffer:
+			await self.cmd_write_mem(self.buf_addr, buffer)
+		return await self.cmd_raw_syscall(sys_no, *int_args)
+
+	async def __aenter__(self) -> Self:
+		await self.connect()
+		return self
+
+	async def __aexit__(self, exc_type, exc_val, exc_tb) -> Optional[bool]:
+		await self.shutdown()
+
+
+async def main():
+	async with BackdClientSession("127.0.0.1", 31337) as sess:
+		print("connected")
+		hello = b"hello, world!\n"
+		res = await sess.cmd_syscall_helper(64, 1, hello, len(hello))
+		print("res:", res)
+
+if __name__ == "__main__":
+	asyncio.run(main())
---
+++ server/python/server.py
@@ -0,0 +1,77 @@
+"""
+This impl mainly exists for testing purposes.
+
+It is single-threaded and accepts only one client at a time.
+"""
+
+import mmap
+import ctypes
+import socket
+
+BUF_SIZE = 0x100000
+
+def recvn(s: socket.socket, n: int):
+	buf = b""
+	while len(buf) < n:
+		part = s.recv(n-len(buf))
+		if not part:
+			raise EOFError
+		buf += part
+	assert(len(buf) == n)
+	return buf
+
+def respond(s: socket.socket, msg: bytes):
+	s.sendall(len(msg).to_bytes(4, "little") + msg)
+
+def main():
+	libc = ctypes.CDLL(None)
+	buffer = mmap.mmap(-1, BUF_SIZE)
+	buffer_addr = ctypes.addressof(ctypes.c_void_p.from_buffer(buffer))
+	print("buffer @", hex(buffer_addr))
+
+	with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.bind(("0.0.0.0", 31337))
+		s.listen(1)
+		conn, addr = s.accept()
+		print("connection from", addr)
+
+		# send server info
+		server_info = BUF_SIZE.to_bytes(4, "little") + buffer_addr.to_bytes(8, "little")
+		respond(conn, server_info)
+
+		while True:
+			try:
+				msg = recvn(conn, 8)
+			except EOFError:
+				break
+			print(msg.hex())
+			cmd_id = int.from_bytes(msg[:2], "little")
+			hdr_len = int.from_bytes(msg[2:4], "little")
+			body_len = int.from_bytes(msg[4:], "little")
+			hdr = recvn(conn, hdr_len)
+			body = recvn(conn, body_len)
+			if cmd_id == 0:
+				assert(hdr_len == 8)
+				addr = int.from_bytes(hdr, "little")
+				buffer[addr-buffer_addr:addr-buffer_addr+len(body)] = body
+				respond(conn, b"")
+			elif cmd_id == 1:
+				assert(hdr_len == 12)
+				addr = int.from_bytes(hdr[:8], "little")
+				length = int.from_bytes(hdr[8:], "little")
+				respond(conn, buffer[addr-buffer_addr:addr-buffer_addr+length])
+			elif cmd_id == 2:
+				sysno = int.from_bytes(hdr[:2], "little")
+				args = [ctypes.c_ulonglong(int.from_bytes(hdr[i:i+8], "little")) for i in range(2, len(hdr), 8)]
+				print("syscall", sysno, [arg for arg in args])
+				res = libc.syscall(sysno, *args) & 0xffff_ffff_ffff_ffff
+				respond(conn, res.to_bytes(8, "little"))
+			else:
+				conn.sendall(b"\xff\xff\xff\xff") # error
+		
+		conn.close()
+		s.close()
+
+if __name__ == "__main__":
+	main()
